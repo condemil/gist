edb78b6c5e489feb2fbccc57ed8b9a66e81a98dc
condemil <condemil@gmail.com>
2011-10-17 01:12:24 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+Gist
+====
+
+[Sublime Text 2](http://www.sublimetext.com/) plugin for creating new Gists from selected text.
+
+Installation
+-----------
+
+Go to your Packages subdirectory under Sublime Text 2 data directory:
+
+* Windows: %APPDATA%\Sublime Text 2
+* OS X: ~/Library/Application Support/Sublime Text 2
+* Linux: ~/.config/sublime-text-2
+* Portable Installation: Sublime Text 2/Data
+
+Then clone this repository:
+
+    git clone git://github.com/condemil/Gist
+
+Options
+-------
+
+Under the Packages/Gist sub-directory, edit the `Gist.sublime-settings` file:
+
+*   `"create_public": false`
+
+    This makes your gists be private instead of public.
+
+*   `"username": YOUR_USERNAME`
+
+    You need to enter your GitHub username here
+
+*   `"password": YOUR_PASSWORD`
+
+    You need to enter your GitHub password here
+
+Usage
+-----
+
+From menu items:
+
+* Main menu: Tools -> "Create Gist"
+* Context menu: "Create Gist"
+
+By command called "Create Gist from Selected Text".
+
+There is a key bindings:
+* Windows and Linux: "ctrl+k", "ctrl+i"
+* OS X: "super+k", "super+i"
+
+Information
+-----------
+
+Source: https://github.com/condemil/Gist
+
+Author: https://github.com/condemil/
---
+++ gist.py
@@ -0,0 +1,51 @@
+import sublime
+import sublime_plugin
+import os
+import json
+import urllib2
+import base64
+
+DEFAULT_CREATE_PUBLIC_VALUE = 'false'
+_selectedText = ''
+_fileName = ''
+
+def create_gist(description):
+	settings = sublime.load_settings('Gist.sublime-settings')
+
+	url = 'https://api.github.com/gists'
+
+	data = json.dumps({ 'description': description,
+						'public': settings.get('create_public', DEFAULT_CREATE_PUBLIC_VALUE),
+						'files': {
+							_fileName: {'content': _selectedText}
+						}})
+
+	headers = { 'Authorization': 'Basic ' + base64.urlsafe_b64encode("%s:%s" % (settings.get('username'), settings.get('password'))),
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				'Content-Length': len(data)}
+
+	request = urllib2.Request(url, data, headers)
+	result = urllib2.urlopen(request)
+
+class PromptGistCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		fileName = os.path.basename(self.window.active_view().file_name())
+		self.window.show_input_panel('File name:', fileName, self.on_done_input_file_name, None, None)
+
+	def on_done_input_file_name(self, fileName):
+		global _fileName
+		_fileName = fileName
+		self.window.show_input_panel('Description (optional):', '', self.on_done_input_description, None, None)
+
+	def on_done_input_description(self, description):
+		create_gist(description)
+
+class GistCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		for selectedRegion in self.view.sel():
+			if not selectedRegion.empty():
+				global _selectedText
+				_selectedText = self.view.substr(selectedRegion)
+				self.view.window().run_command('prompt_gist')
+
