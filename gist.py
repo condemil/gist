53f37d29fccfd572a358beca2eef8e65b2ce9d65
Eric Ponce <tricokun@gmail.com>
2011-12-15 01:42:53 +0100

    Add list gist

--- gist.py
+++ gist.py
@@ -8,6 +8,7 @@ import base64
 DEFAULT_CREATE_PUBLIC_VALUE = 'false'
 _selectedText = ''
 _fileName = ''
+newlist2 = []
 
 def create_gist(description):
 	settings = sublime.load_settings('Gist.sublime-settings')
@@ -39,6 +40,64 @@ def create_gist(description):
 	result = json.loads(response.read())
 	sublime.set_clipboard(result['html_url'])
 
+def get_gist(id):
+	settings = sublime.load_settings('Gist.sublime-settings')
+
+	url = id
+
+	request = urllib2.Request(url)
+
+	request.add_header('Authorization', 'Basic ' + base64.urlsafe_b64encode("%s:%s" % (settings.get('username'), settings.get('password'))))
+	request.add_header('Accept', 'application/json')
+	request.add_header('Content-Type', 'application/json')
+
+	if settings.get('use_proxy') == 'true':
+		opener = urllib2.build_opener(
+            urllib2.HTTPHandler(),
+            urllib2.HTTPSHandler(),
+            urllib2.ProxyHandler({'https': settings.get('proxy')}))        
+		urllib2.install_opener(opener)
+		
+	response = urllib2.urlopen(request)
+
+	result = json.loads(response.read())
+	for x in result['files']:
+		sublime.set_clipboard(result['files'][x]['content'])
+
+def get_gists():
+	settings = sublime.load_settings('Gist.sublime-settings')
+
+	url = 'https://api.github.com/gists'
+
+	request = urllib2.Request(url)
+	request.add_header('Authorization', 'Basic ' + base64.urlsafe_b64encode("%s:%s" % (settings.get('username'), settings.get('password'))))
+	request.add_header('Accept', 'application/json')
+	request.add_header('Content-Type', 'application/json')
+
+	if settings.get('use_proxy') == 'true':
+		opener = urllib2.build_opener(
+            urllib2.HTTPHandler(),
+            urllib2.HTTPSHandler(),
+            urllib2.ProxyHandler({'https': settings.get('proxy')}))        
+		urllib2.install_opener(opener)
+		
+	response = urllib2.urlopen(request)
+
+	result = json.loads(response.read())
+
+	newlist = []
+	global newlist2
+	for x in result:
+		if(x['description'] != None):
+			newlist.append([x['description']])			
+		else: 
+			newlist.append(['No named'])
+		
+		newlist2.append([x['url']])
+	
+	return newlist
+
+
 class PromptGistCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		fileName = os.path.basename(self.window.active_view().file_name()) if self.window.active_view().file_name() else ''
@@ -50,7 +109,7 @@ class PromptGistCommand(sublime_plugin.WindowCommand):
 		self.window.show_input_panel('Description (optional):', '', self.on_done_input_description, None, None)
 
 	def on_done_input_description(self, description):
-		create_gist(description)
+		create_gist2(description)
 
 class GistCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
@@ -62,3 +121,11 @@ class GistCommand(sublime_plugin.TextCommand):
 			else:
 				_selectedText = self.view.substr(sublime.Region(0, self.view.size()))
 				self.view.window().run_command('prompt_gist')
+
+class GistlistCommand(sublime_plugin.WindowCommand):
+	def run(self):	
+			
+		self.window.show_quick_panel(get_gists(), self.on_done)
+	
+	def on_done(self, num):
+		get_gist(newlist2[num][0])
