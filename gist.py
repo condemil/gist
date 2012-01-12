055614ef63a05a3ea2980feffd16f78910a9cd14
Alexey Ermakov <zee@technocore.ru>
2012-01-12 23:30:05 +0400

    OS X: use credentials from keychain.
    On OS X, if username and password (or just password) haven't been set in
    Gist.sublime-settings, try fetching missing information from the user's
    github.com keychain entry (assuming he has one).

--- gist.py
+++ gist.py
@@ -6,6 +6,7 @@ import json
 import base64
 import urllib2
 import subprocess
+import functools
 
 DEFAULT_CREATE_PUBLIC_VALUE = 'false'
 DEFAULT_USE_PROXY_VALUE = 'false'
@@ -15,23 +16,108 @@ _gistsUrls = []
 settings = sublime.load_settings('Gist.sublime-settings')
 url = 'https://api.github.com/gists'
 
-def check_settings():
-    if not settings.get('username') or not settings.get('password'):
-        sublime.status_message('GitHub username or password doesn\'t provided in Gist.sublime-settings file')
-        return False
+class GistMissingCredentialsException(Exception):
+    pass
 
-    return True
+def get_credentials():
+    username = settings.get('username')
+    password = settings.get('password')
+    if not username or not password:
+        raise GistMissingCredentialsException()
+    return (username, password)
+
+if sublime.platform() == 'osx':
+    # Keychain support
+    # Instead of Gist.sublime-settings, fetch username and password from the user's github.com keychain entry
+    SERVER = 'github.com'
+
+    def create_keychain_accessor():
+        from ctypes import cdll, util, c_uint32, c_int, c_char_p, c_void_p, POINTER, pointer, byref, Structure, string_at
+        lib_security = cdll.LoadLibrary(util.find_library('Security'))
+
+        class SecKeychainAttributeInfo(Structure):
+            _fields_ = [("count", c_uint32), ("tag", POINTER(c_uint32)), ("format", POINTER(c_uint32))]
+
+        class SecKeychainAttribute(Structure):
+            _fields_ = [("tag", c_uint32), ("length", c_uint32), ("data", c_void_p)]
+
+        class SecKeychainAttributeList(Structure):
+            _fields_ = [("count", c_uint32), ("attr", POINTER(SecKeychainAttribute))]
+
+        def keychain_get_credentials():
+            username = settings.get('username')
+            password = settings.get('password')
+            if username and password:
+                return (username, password)
+
+            password_buflen = c_uint32()
+            password_buf = c_void_p()
+            item = c_void_p()
+
+            error = lib_security.SecKeychainFindInternetPassword(
+               None,
+               c_uint32(len(SERVER)),
+               c_char_p(SERVER),
+               c_uint32(0),
+               None,
+               c_uint32(0 if not username else len(username)),
+               None if not username else c_char_p(username),
+               c_uint32(0),
+               None,
+               c_uint32(0),
+               c_int(0), # kSecProtocolTypeAny
+               c_int(0), # kSecAuthenticationTypeAny
+               None,
+               None,
+               byref(item))
+            if not error:
+                info = SecKeychainAttributeInfo()
+                info.count = 1
+                info.tag = pointer(c_uint32(1633903476)) # kSecAccountItemAttr
+                info.format = pointer(c_uint32(6)) # CSSM_DB_ATTRIBUTE_FORMAT_BLOB
+
+                attrlist_ptr = pointer(SecKeychainAttributeList())
+                error = lib_security.SecKeychainItemCopyAttributesAndData(
+                    item,
+                    byref(info),
+                    None,
+                    byref(attrlist_ptr),
+                    byref(password_buflen),
+                    byref(password_buf))
+
+                if not error:
+                    try:
+                        if attrlist_ptr.contents.count == 1:
+                            attr = attrlist_ptr.contents.attr.contents
+                            username = string_at(attr.data, attr.length)
+                            password = string_at(password_buf.value, password_buflen.value)
+                    finally:
+                        lib_security.SecKeychainItemFreeContent(attrlist_ptr, password_buf)
+
+            if not username or not password:
+                raise GistMissingCredentialsException()
+            else:
+                return (username, password)
+
+        return keychain_get_credentials
+    get_credentials = create_keychain_accessor()
+
+def catching_credential_errors(fn):
+    @functools.wraps(fn)
+    def _fn(*args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        except GistMissingCredentialsException:
+            sublime.error_message("GitHub username or password isn't provided in Gist.sublime-settings file")
+    return _fn
 
 def create_gist(description, public):
     data = json.dumps({ 'description': description, 'public': public, 'files': { _fileName: {'content': _selectedText} }})
-
     result = api_request(url, data)
-
     sublime.set_clipboard(result['html_url'])
 
 def get_gist(url_gist):
     gists = api_request(url_gist)
-
     for gist in gists['files']:
         sublime.set_clipboard(gists['files'][gist]['content'])
 
@@ -55,7 +141,7 @@ def api_request(url_api, data = ''):
         return api_request_wget(url_api, data)
 
     request = urllib2.Request(url_api)
-    request.add_header('Authorization', 'Basic ' + base64.urlsafe_b64encode("%s:%s" % (settings.get('username'), settings.get('password'))))
+    request.add_header('Authorization', 'Basic ' + base64.urlsafe_b64encode("%s:%s" % get_credentials()))
     request.add_header('Accept', 'application/json')
     request.add_header('Content-Type', 'application/json')
 
@@ -85,7 +171,7 @@ def api_request_wget(url_api, data = ''):
     if (not wget):
         return False
 
-    authorization_string = "%s:%s" % (settings.get('username'), settings.get('password'))
+    authorization_string = "%s:%s" % get_credentials()
 
     command = [wget, '-O', '-', '-q']
     command.append('--header=Authorization: Basic ' + base64.urlsafe_b64encode(authorization_string))
@@ -113,7 +199,9 @@ def api_request_wget(url_api, data = ''):
     return json.loads(response)
 
 class PromptPublicGistCommand(sublime_plugin.WindowCommand):
+    @catching_credential_errors
     def run(self):
+        get_credentials()
         fileName = os.path.basename(self.window.active_view().file_name()) if self.window.active_view().file_name() else ''
         self.window.show_input_panel('Public Gist File Name: (optional):', fileName, self.on_done_input_file_name, None, None)
 
@@ -122,11 +210,14 @@ class PromptPublicGistCommand(sublime_plugin.WindowCommand):
         _fileName = fileName
         self.window.show_input_panel('Public Gist Description (optional):', '', self.on_done_input_description, None, None)
 
+    @catching_credential_errors
     def on_done_input_description(self, description):
         create_gist(description, "true")
 
 class PromptPrivateGistCommand(sublime_plugin.WindowCommand):
+    @catching_credential_errors
     def run(self):
+        get_credentials()
         fileName = os.path.basename(self.window.active_view().file_name()) if self.window.active_view().file_name() else ''
         self.window.show_input_panel('Private Gist File Name: (optional):', fileName, self.on_done_input_file_name, None, None)
 
@@ -135,38 +226,38 @@ class PromptPrivateGistCommand(sublime_plugin.WindowCommand):
         _fileName = fileName
         self.window.show_input_panel('Private Gist Description (optional):', '', self.on_done_input_description, None, None)
 
+    @catching_credential_errors
     def on_done_input_description(self, description):
         create_gist(description, "false")
 
 class GistCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        if(check_settings()):
-            for selectedRegion in self.view.sel():
-                if not selectedRegion.empty():
-                    global _selectedText
-                    _selectedText = self.view.substr(selectedRegion)
-                    self.view.window().run_command('prompt_public_gist')
-                else:
-                    _selectedText = self.view.substr(sublime.Region(0, self.view.size()))
-                    self.view.window().run_command('prompt_public_gist')
+        for selectedRegion in self.view.sel():
+            if not selectedRegion.empty():
+                global _selectedText
+                _selectedText = self.view.substr(selectedRegion)
+                self.view.window().run_command('prompt_public_gist')
+            else:
+                _selectedText = self.view.substr(sublime.Region(0, self.view.size()))
+                self.view.window().run_command('prompt_public_gist')
 
 class GistPrivateCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        if(check_settings()):
-            for selectedRegion in self.view.sel():
-                if not selectedRegion.empty():
-                    global _selectedText
-                    _selectedText = self.view.substr(selectedRegion)
-                    self.view.window().run_command('prompt_private_gist')
-                else:
-                    _selectedText = self.view.substr(sublime.Region(0, self.view.size()))
-                    self.view.window().run_command('prompt_private_gist')
+        for selectedRegion in self.view.sel():
+            if not selectedRegion.empty():
+                global _selectedText
+                _selectedText = self.view.substr(selectedRegion)
+                self.view.window().run_command('prompt_private_gist')
+            else:
+                _selectedText = self.view.substr(sublime.Region(0, self.view.size()))
+                self.view.window().run_command('prompt_private_gist')
 
 class GistListCommand(sublime_plugin.WindowCommand):
+    @catching_credential_errors
     def run(self):
-        if(check_settings()):
-            gists = get_gists()
-            self.window.show_quick_panel(gists, self.on_done)
+        gists = get_gists()
+        self.window.show_quick_panel(gists, self.on_done)
 
+    @catching_credential_errors
     def on_done(self, num):
         get_gist(_gistsUrls[num][0])
