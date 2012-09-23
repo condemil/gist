fa662adbf69f7f535ca01a693abcb79a7c9371a7
UncleBill <billbill290@gmail.com>
2012-09-23 11:14:58 +0800

    Update gist.py
    set max_gists 100

--- gist.py
+++ gist.py
@@ -30,6 +30,7 @@ if settings.get('max_gists'):
     if settings.get('max_gists') <= 100:
         GISTS_URL += '?per_page=%d' % settings.get('max_gists'); 
     else:
+        settings.set("max_gists",100)
         sublime.error_message("Gist: GitHub API does not support a value of higher than 100")
 
 class MissingCredentialsException(Exception):
