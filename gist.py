5432d25922e8b7d2af285ca52e27d42804e5e44f
Brandon Rubin <Steelsouls@gmail.com>
2013-05-06 17:32:50 -0700

    Refine file_name code to match project style

--- gist.py
+++ gist.py
@@ -288,8 +288,7 @@ def get_user_gists(user):
     return api_request(USER_GISTS_URL % user)
 
 def gist_title(gist):
-    files = gist.get('files')
-    file_name = list(files)
+    file_name = sorted(gist['files'].keys())
     title = file_name[0] or gist.get('id')
 
     if settings.get('show_authors'):
